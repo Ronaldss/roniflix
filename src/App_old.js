f60ffa92aad88fc1d0d00d06e4a06e8db8b839ad
Ronald Santos <ronaldcontact2019@gmail.com>
2020-10-16 01:02:11 -0300

    :spring: font correction

---
+++ src/App_old.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "./components/Menu";
+import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
