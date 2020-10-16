e6b101aed47397401eb5b786ece3851947abf5d6
Ronald Santos <ronaldcontact2019@gmail.com>
2020-10-16 01:15:15 -0300

    :seedling: text painel correction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import React from "react";
+import Menu from "./components/Menu";
+import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,33 +13,22 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={
+          "Era o judaísmo, nesse período, uma religião dos hebreus. Vamos verificar como funcionava a civilização hebraica, que tantas influências nos deixou?"
+        }
       />
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+      <Carousel category={dadosIniciais.categorias[1]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
+      <Carousel category={dadosIniciais.categorias[2]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
+      <Carousel category={dadosIniciais.categorias[3]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
+      <Carousel category={dadosIniciais.categorias[4]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
+      <Carousel category={dadosIniciais.categorias[5]} />
 
       <Footer />
     </div>
